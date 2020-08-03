723c838fa5c505e9fc851f3e715530079f93e525
Vachounet <vachounet@live.fr>
2020-08-03 21:13:09 +0200

    sofiar: add back common firmware loading
    * Some variant(s) are using Novatek panel
    * Fix firmware loading on boot

--- recovery/root/sbin/load_ts_firmware.sh
+++ recovery/root/sbin/load_ts_firmware.sh
@@ -1,14 +1,32 @@
 #!/sbin/sh
 
-
-SLOT=`getprop ro.boot.slot_suffix`
-bootmode=$(getprop ro.boot.fastboot)
-
 module_path=/sbin/modules
 
 touch_class_path=/sys/class/touchscreen
 touch_path=
 firmware_path=/vendor/firmware
+
+wait_for_poweron()
+{
+	local wait_nomore
+	local readiness
+	local count
+	wait_nomore=60
+	count=0
+	while true; do
+		readiness=$(cat $touch_path/poweron)
+		if [ "$readiness" == "1" ]; then
+			break;
+		fi
+		count=$((count+1))
+		[ $count -eq $wait_nomore ] && break
+		sleep 1
+	done
+	if [ $count -eq $wait_nomore ]; then
+		return 1
+	fi
+	return 0
+}
 
 # Load all needed modules
 insmod $module_path/sensors_class.ko
@@ -23,15 +41,19 @@ insmod $module_path/moto_f_usbnet.ko
 insmod $module_path/qpnp-power-on-mmi.ko
 insmod $module_path/qpnp-smbcharger-mmi.ko
 insmod $module_path/tas2562.ko
-insmod $module_path/nova_0flash_mmi.ko
 insmod $module_path/focaltech_0flash_mmi.ko
+insmod $module_path/nova_0flash_mmi.ko
 
 cd $firmware_path
-touch_product_string="ft8756"
-touch_path="/sys/devices/platform/soc/4a88000.spi/spi_master/spi0/spi0.0"
-firmware_file="focaltech-ft8756-0d-01-sofiar.bin"
-echo 1 > $touch_path/forcereflash
-echo $firmware_file > $touch_path/doreflash
-
+for touch_product_string in $(ls $touch_class_path); do
+    touch_path=/sys$(cat $touch_class_path/$touch_product_string/path | awk -Fsofia '{print $1}')
+    firmware_file=$(ls *$touch_product_string*)
+    wait_for_poweron
+    echo $firmware_file > $touch_path/doreflash
+    echo 1 > $touch_path/forcereflash
+    sleep 5
+    echo 1 > $touch_path/reset
+done
 
 return 0
+
