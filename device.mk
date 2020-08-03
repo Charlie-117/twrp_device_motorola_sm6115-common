c6f0e7eb85f765927598de01f96080e34a879e3b
Vachounet <vachounet@live.fr>
2020-08-03 17:32:34 +0200

    sofiar: fix decryption

--- device.mk
+++ device.mk
@@ -36,4 +36,7 @@ PRODUCT_HOST_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/recovery.img-dtb:dtb.img
+
 #PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root)
